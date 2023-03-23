4ef150b77d8a74fa016b7e0f7f7778d5bbb109e4
rlaecio <rlaecio@hotmail.com>
2023-03-23 16:16:59 +0000

    map

--- app/main.js
+++ app/main.js
@@ -2,27 +2,11 @@ let livros = [];
 const endPointApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
 getBuscarLivrosApi();
-const elementoParaInserirLivros = document.getElementById('livros');
-
 
 async function getBuscarLivrosApi() {
     const res = await fetch(endPointApi);
     livros = await res.json();
-    console.table(livros);
-    exibirOslivrosNaTela(livros);
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirOslivrosNaTela(livrosComDesconto);
 }
 
-function exibirOslivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo"> ${livro.titulo} </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div> `
-    })
-}
