e80bb6c76ab4de68f76f9f41e716bf6be15c706a
rlaecio <rlaecio@hotmail.com>
2023-03-23 18:44:33 +0000

    sort

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,15 +3,26 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOslivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML ='';
     listaDeLivros.forEach(livro => {
+        let disponibilidade = verificaDisponibilidadeLivro(livro);
+
         elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo"> ${livro.titulo} </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div> `
+            <div class="livro">
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+                <h2 class="livro__titulo"> ${livro.titulo} </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+                <div class="tags">
+                  <span class="tag">${livro.categoria}</span>
+                </div>
+            </div> `
     })
 }
+
+function verificaDisponibilidadeLivro(livros) {
+    if (livros.quantidade > 0 ) {
+        return 'livro__imagens';
+    } else {
+        return 'livro__imagens indisponivel';
+    }
+}
+
